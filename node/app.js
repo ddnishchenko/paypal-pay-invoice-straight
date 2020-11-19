71d2ea4b651875a767fa791a7d3bfc31efdf1ea5
ddnishchenko <ddnishchenko@itera-research.com>
2020-11-19 17:56:41 +0200

    first commit

---
+++ node/app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware');
+var cors = require('cors');
+var timeout = require('connect-timeout');
+
+var indexRouter = require('./routes/index');
+var invoicesRouter = require('./routes/invoices');
+
+var app = express();
+
+// timeout
+app.use(timeout('1h'))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: false}));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/models', modelsRouter);
+app.use('/invoices', invoicesRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ node/routes/invoices.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var url = require('url');
+var router = express.Router();
+const puppeteer = require('puppeteer');
+
+router.get('/:invoice', async function(req, res, next) {
+    
+    const invoice = req.params.invoice;
+    console.log(invoice);
+    const browser = await puppeteer.launch({
+        args: [
+            // Required for Docker version of Puppeteer
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            // This will write shared memory files into /tmp instead of /dev/shm,
+            // because Docker’s default for /dev/shm is 64MB
+            '--disable-dev-shm-usage'
+          ]
+    });
+    let pages = await browser.pages();
+    const page = await browser.newPage();
+    browser.on('targetcreated', async () => {
+        try {
+            console.log('New Tab Created');
+            let uri;
+            pages = await browser.pages();
+            console.log('pages', pages.length);
+            const paypalWindow = pages[pages.length - 1];
+            await paypalWindow.waitForTimeout(3000);
+            const cnt = await paypalWindow.content();
+            uri = paypalWindow.url();
+            await paypalWindow.waitForSelector('.button.secondary', {timeout: 1000 * 60});
+            await paypalWindow.click('.button.secondary');
+            await paypalWindow.waitForTimeout(3000);
+            // await paypalWindow.waitForSelector('#countrySelector', {timeout: 1000 * 60});
+            // await paypalWindow.select('#countrySelector', 'CA');
+            await paypalWindow.waitForTimeout(3000);
+            uri = paypalWindow.url();
+            console.log(uri);
+            const parsedUrl = url.parse(uri, true);
+            const finalUri = `https://www.paypal.com/webapps/xoonboarding?token=${parsedUrl.query.token}&useraction=commit&country.x=CA&locale.x=en_US#/checkout/guest`;
+            res.redirect(finalUri);
+        } catch (e) {
+            res.json({error: e.message});
+        }
+        await browser.close();
+        
+    })
+    try {
+        await page.goto('https://www.paypal.com/invoice/payerView/details/'+invoice);
+        await page.waitForSelector('#payInvoiceInPopup');
+        await page.click('#payInvoiceInPopup');
+    } catch (e) {
+        res.json({error: e});
+    }
+    
+    
+});
+
+module.exports = router;
